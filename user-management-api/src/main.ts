0c370f6d99525f9c3982ad83bc33082a8488a758
Jose Gallego <josecarva16@gmail.com>
2025-08-20 12:04:38 -0500

    Update api documentation

--- user-management-api/src/main.ts
+++ user-management-api/src/main.ts
@@ -8,25 +8,6 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configuración de Swagger
-  const config = new DocumentBuilder()
-    .setTitle('E-commerce API')
-    .setDescription('API for managing users, products, and purchases')
-    .setVersion('1.0')
-    .addBearerAuth() // Para endpoints protegidos con JWT
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  
-  SwaggerModule.setup('api', app, document); // Sirve Swagger UI en /api (opcional)
-
-  // Exponer el archivo OpenAPI JSON
-  app.getHttpAdapter().get('/api-json', (req, res) => {
-    res.json(document);
-  });
-
-  // Guardar openapi.json en la carpeta docs
-  writeFileSync(join(__dirname, '..', 'docs', 'openapi.json'), JSON.stringify(document, null, 2));
-
   app.enableCors({
     origin: "http://localhost:4200",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
